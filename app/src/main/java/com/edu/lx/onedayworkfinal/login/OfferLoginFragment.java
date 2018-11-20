a989f18b7830295e8d12980c2fa0e9d62006a34a
KimDongGa <dgkim0419@gmail.com>
2018-11-20 23:16:05 +0900

    KimDongGa - Alter FindInfoActivity

--- app/src/main/java/com/edu/lx/onedayworkfinal/login/OfferLoginFragment.java
+++ app/src/main/java/com/edu/lx/onedayworkfinal/login/OfferLoginFragment.java
@@ -46,8 +46,8 @@ public class OfferLoginFragment extends Fragment {
         offerIdInput = rootView.findViewById(R.id.offerIdInput);
         offerPwInput = rootView.findViewById(R.id.offerPwInput);
 
-        Button seekerLoginButton = rootView.findViewById(R.id.offerLoginButton);
-        seekerLoginButton.setOnClickListener(v -> loginOffer());
+        Button offerLoginButton = rootView.findViewById(R.id.offerLoginButton);
+        offerLoginButton.setOnClickListener(v -> loginOffer());
         return rootView;
     }
 
--- app/src/main/java/com/edu/lx/onedayworkfinal/login/findInfo/OfferPwAlterActivity.java
+++ app/src/main/java/com/edu/lx/onedayworkfinal/login/findInfo/OfferPwAlterActivity.java
@@ -1,15 +1,46 @@
 package com.edu.lx.onedayworkfinal.login.findInfo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.edu.lx.onedayworkfinal.R;
+import com.edu.lx.onedayworkfinal.login.LoginActivity;
 
 public class OfferPwAlterActivity extends AppCompatActivity {
+
+    EditText offerPwNewInput;
+    EditText offerPwReInput;
+    Button offerPwCheckButton;
+    Button gotoLoginPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_pw_alter);
+
+        offerPwNewInput = findViewById(R.id.offerPwNewInput);
+        offerPwReInput = findViewById(R.id.offerPwReInput);
+        offerPwCheckButton = findViewById(R.id.offerPwCheckButton);
+        gotoLoginPage = findViewById(R.id.gotoLoginPage);
+
+        offerPwCheckButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        gotoLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OfferPwAlterActivity.this,LoginActivity.class);
+                startActivityForResult(intent,410);
+            }
+        });
     }
 }
